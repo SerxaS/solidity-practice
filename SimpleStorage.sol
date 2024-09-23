73f2a72ee6fc1109d9132af091011153ea59ce97
SerxaS <serhas@gmail.com>
2024-09-23 17:09:15 +0300

    SimpleStorage changed.

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity 0.8.26;
 
 contract SimpleStorage {
@@ -9,14 +10,21 @@ contract SimpleStorage {
        string name; 
     }
 
-    Person public myFriend = Person({favoriteNumber: 7, name: "Pat"});
+Person[] public listOfPeople;
 
-    function store(uint256 favnum) public {
-        myFavoriteNumber = favnum;
+mapping (string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
     }
 
     function retrieve() public view returns(uint256) {
         return myFavoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public  {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 
 }
