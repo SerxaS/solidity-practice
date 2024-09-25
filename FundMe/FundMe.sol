5abd2eec7d70eebe981b8acfc6f5329a789f21c9
SerxaS <serhas@gmail.com>
2024-09-25 10:40:10 +0300

    Fundme contract add.

---
+++ FundMe/FundMe.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.26;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+contract FundMe {
+
+    uint256 public minimumUsd = 5e18;    
+
+    function fund() public payable {
+        require(getConversionRate(msg.value) >= minimumUsd, "Didn't send enough ETH");
+    }                
+
+    function getPrice() public view returns(uint256) { 
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);      
+        (, int answer,,,) = priceFeed.latestRoundData();
+        return uint256(answer * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return  ethAmountInUsd;
+    }
+}
